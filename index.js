71dff263bcb3312272b8111375399085dd73bcef
zaidi mounib <mounibzaidi@gmail.com>
2023-08-16 17:25:46 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+    if (button) {
+        button.addEventListener('click', function () {
+            alert('Button clicked!');
+        });
+    } else {
+        console.error('Button element not found.');
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    addingEventListener();
+});
